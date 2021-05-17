a4ca30b64bd32dba7901ffbb200f72f2e82c4de4
lidan65 <lidan65@meituan.com>
2021-05-17 21:30:08 +0800

    add distill logit part

--- 6.sbert_distill_logit.sh
+++ 6.sbert_distill_logit.sh
@@ -23,7 +23,7 @@ python sbert_distill.py \
   --train_batch_size=64 \
   --learning_rate=1e-5 \
   --test_flie_name=None \
-  --num_train_epochs=0.1 \
+  --num_train_epochs=0.001 \
   --do_save=false \
   --output_dir=$OUTPUT \
   --use_kd_logit=true \
