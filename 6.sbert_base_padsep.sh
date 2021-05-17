c3e54a18147281e77e86f9dbfceb016b6000917d
lidan65 <lidan65@meituan.com>
2021-05-17 17:31:10 +0800

    add distill logit part

--- 6.sbert_base_padsep.sh
+++ 6.sbert_base_padsep.sh
@@ -5,7 +5,7 @@ export CUDA_VISIBLE_DEVICES=1
 export BERT_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/english_bert_base_model/uncased_L-12_H-768_A-12
 export MY_DATASET=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/dataset/mnli/ #全局变量 数据集所在地址
 #export OUTPUT=./output/search_spuall_3/
-export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/s_bert_base_pad_sep/
+export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/s_bert_base_pad_sep_realpool/
 python s_bert.py \
   --task_name=mnli \
   --pooling_strategy=mean \
