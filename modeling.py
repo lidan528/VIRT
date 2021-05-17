3061562e3f276e9292237d48ed6b6738c65e7af2
lidan65 <lidan65@meituan.com>
2021-05-17 20:30:35 +0800

    add distill logit part

--- modeling.py
+++ modeling.py
@@ -412,7 +412,7 @@ def get_assignment_map_from_checkpoint_student(tvars, init_checkpoint):
   assignment_map = collections.OrderedDict()
   for x in init_vars:
     (name, var) = (x[0], x[1])
-    graph_bert_name = 'bert_student/'+name[:4]
+    graph_bert_name = 'bert_student/'+name[4:]
     if graph_bert_name in name_to_variable:
       assignment_map[name] = graph_bert_name
       initialized_variable_names[name] = 1
