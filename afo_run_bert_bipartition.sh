830126735b443312ae1b8f599db867675618510e
lidan65 <lidan65@meituan.com>
2021-05-20 17:22:46 +0800

    add distill logit part

--- afo_run_bert_bipartition.sh
+++ afo_run_bert_bipartition.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/meituan/hadoop-gpu/bin/hadoop_user_login_centos7.sh hadoop-aipnlp
 source /opt/meituan/tensorflow-release/local_env.sh
-/opt/meituan/tensorflow-release/bin/mpi-submit.sh -conf /home/sankuai/sankuai/clue/classify/bert_base_bipartition.xml -files /opt/meituan/lidan65/distill
+/opt/meituan/tensorflow-release/bin/mpi-submit.sh -conf /opt/meituan/lidan65/distill/bert_base_bipartition.xml -files /opt/meituan/lidan65/distill
