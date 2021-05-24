b6915fbaff55ace22c667f65fb90c38f0c392141
lidan65 <lidan65@meituan.com>
2021-05-24 14:38:49 +0800

    add distill logit part

---
+++ 6.sbert_distill_att_100.sh
@@ -0,0 +1,34 @@
+export CUDA_VISIBLE_DEVICES=2
+#export BERT_BASE_DIR=/home/hadoop-aipnlp/cephfs/data/transfer_to_zw03/yangyang113/bert_base
+#export BERT_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/bujiahao/outstanding_ckpt/128_model
+#export BERT_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/transfer_to_zw03/yangyang113/bert_base/train_log/bert_distil_minilm_L6h128I512A2_common128_wa
+export BERT_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/english_bert_base_model/uncased_L-12_H-768_A-12
+export TEACHER_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/bert_base_bipartition_mean_pool
+export MY_DATASET=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/dataset/mnli/ #全局变量 数据集所在地址
+#export OUTPUT=./output/search_spuall_3/
+export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/s_bert_distll_att_100/
+python sbert_distill.py \
+  --task_name=mnli \
+  --pooling_strategy=mean \
+  --do_train=true \
+  --do_eval=true \
+  --do_predict=false \
+  --data_dir=$MY_DATASET \
+  --vocab_file=$BERT_MODEL_DIR/vocab.txt \
+  --bert_config_file=$BERT_MODEL_DIR/bert_config.json \
+  --init_checkpoint_teacher=$TEACHER_MODEL_DIR/model.ckpt-43000 \
+  --init_checkpoint_student=$BERT_MODEL_DIR/bert_model.ckpt \
+  --max_seq_length_bert=259 \
+  --max_seq_length_sbert=130 \
+  --train_batch_size=32 \
+  --learning_rate=5e-5 \
+  --test_flie_name=None \
+  --num_train_epochs=6 \
+  --do_save=false \
+  --output_dir=$OUTPUT \
+  --use_kd_logit_kl=false \
+  --use_kd_logit_mse=false \
+  --kd_weight_logit=0 \
+  --use_kd_att=true \
+  --kd_weight_att=100
+
