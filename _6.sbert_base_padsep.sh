86784c7442961af2790065b7054b83f72c5fe85f
lidan65 <lidan65@meituan.com>
2021-05-28 17:29:59 +0800

    add distill logit part

--- _6.sbert_base_padsep.sh
+++ _6.sbert_base_padsep.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=1
 export BERT_BASE_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/english_bert_base_model/uncased_L-12_H-768_A-12
 export MY_DATASET=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/dataset/squad #全局变量 数据集所在地址
 export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/squad/sbert_base_meanpool/
-python _run_classifier.py \
+python _sbert.py \
   --task_name=squad \
   --train_file=$MY_DATASET/squad_v1.1-train.jsonl \
   --dev_file=$MY_DATASET/squad_v1.1-dev.jsonl \
