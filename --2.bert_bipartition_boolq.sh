0f85f4680cc1b3439716c315464962f4765acaf6
lidan65 <lidan65@meituan.com>
2021-06-02 19:57:42 +0800

    add distill logit part

---
+++ --2.bert_bipartition_boolq.sh
@@ -0,0 +1,20 @@
+export CUDA_VISIBLE_DEVICES=3
+export BERT_BASE_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/english_bert_base_model/uncased_L-12_H-768_A-12
+export MY_DATASET=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/dataset/boolq/ #全局变量 数据集所在地址
+export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/boolq/bert_base_bipartition_mean_pool/
+python run_classifier_bipartition.py \
+  --task_name=boolq \
+  --do_train=true \
+  --do_eval=true \
+  --do_predict=false \
+  --data_dir=$MY_DATASET \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --max_seq_length_query=64 \
+  --max_seq_length_doc=328 \
+  --train_batch_size=40 \
+  --learning_rate=5e-5 \
+  --num_train_epochs=6.0 \
+  --output_dir=$OUTPUT \
+  --pooling_strategy=mean
