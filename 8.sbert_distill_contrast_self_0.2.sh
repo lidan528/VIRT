a940c50a732c84ce7e5185a725cceae6537bcaf5
lidan65 <lidan65@meituan.com>
2021-06-03 17:46:05 +0800

    add distill logit part

--- 8.sbert_distill_contrast_self_0.2.sh
+++ 8.sbert_distill_contrast_self_0.2.sh
@@ -6,7 +6,7 @@ export BERT_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/english_be
 export TEACHER_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/bert_base_bipartition_mean_pool
 export MY_DATASET=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/dataset/mnli/ #全局变量 数据集所在地址
 #export OUTPUT=./output/search_spuall_3/
-export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/s_bert_distll_contrast_self_1.0/
+export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/s_bert_distll_contrast_self_0.2/
 python sbert_distill.py \
   --task_name=mnli \
   --pooling_strategy=mean \
