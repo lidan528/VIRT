c0097231b193aa75d98ee05593ca2b21f0640c55
lidan65 <lidan65@meituan.com>
2021-06-05 18:50:04 +0800

    add a .sh

--- eval_flops_bert.py
+++ eval_flops_bert.py
@@ -896,5 +896,5 @@ if __name__ == "__main__":
   flags.mark_flag_as_required("vocab_file")
   flags.mark_flag_as_required("bert_config_file")
   flags.mark_flag_as_required("output_dir")
-  flags.mark_flag_as_required("pooling_strategy")
+  # flags.mark_flag_as_required("pooling_strategy")
   tf.app.run()
