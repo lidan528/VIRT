8ff5f673ed2aa5cebcc973c82fea920e595d60b6
tanghongyin <tanghongyin@meituan.com>
2021-06-07 21:02:59 +0800

    add colbert_mrc

--- colbert_mrc.sh
+++ colbert_mrc.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=3
 export BERT_BASE_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/english_bert_base_model/uncased_L-12_H-768_A-12
 export MY_DATASET=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/dataset/squad #全局变量 数据集所在地址
 colbert_dim=128
-export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/squad/colbert_${poly_first_m}_meanpool/
+export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/squad/colbert_${colbert_dim}_meanpool/
 python colbert_mrc.py \
   --task_name=squad \
   --train_file=$MY_DATASET/squad_v1.1-train.jsonl \
