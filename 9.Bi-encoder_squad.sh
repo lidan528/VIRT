0e1e3f1f4fb87ff6580ff1800c8ca785a9d4151f
lidan65 <lidan65@meituan.com>
2021-06-08 15:12:06 +0800

    add a .sh

---
+++ 9.Bi-encoder_squad.sh
@@ -0,0 +1,20 @@
+export CUDA_VISIBLE_DEVICES=1
+export BERT_BASE_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/english_bert_base_model/uncased_L-12_H-768_A-12
+export MY_DATASET=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/dataset/squad #全局变量 数据集所在地址
+export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/squad/Bi-encoder/
+python Bi-encoder_squad.py \
+  --task_name=squad \
+  --train_file=$MY_DATASET/squad_v1.1-train.jsonl \
+  --dev_file=$MY_DATASET/squad_v1.1-dev.jsonl \
+  --do_train=true \
+  --do_eval=true \
+  --do_predict=false \
+  --data_dir=$MY_DATASET \
+  --vocab_file=$BERT_BASE_DIR/vocab.txt \
+  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+  --train_batch_size=32 \
+  --learning_rate=5e-5 \
+  --num_train_epochs=6.0 \
+  --output_dir=$OUTPUT \
+  --pooling_strategy=mean
