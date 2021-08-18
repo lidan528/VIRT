8ac6d8a0aca1925367c16204c6a6469a1130b984
lidan65 <lidan65@meituan.com>
2021-08-18 11:28:20 +0800

    add a .sh

--- test_weight_att_distill_head.sh
+++ test_weight_att_distill_head.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=0
 #export BERT_BASE_DIR=/home/hadoop-aipnlp/cephfs/data/transfer_to_zw03/yangyang113/bert_base
 #export BERT_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/bujiahao/outstanding_ckpt/128_model
 #export BERT_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/transfer_to_zw03/yangyang113/bert_base/train_log/bert_distil_minilm_L6h128I512A2_common128_wa
@@ -6,7 +6,7 @@ export BERT_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/english_be
 export TEACHER_MODEL_DIR=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/bert_base_bipartition_mean_pool
 export MY_DATASET=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/dataset/mnli/ #全局变量 数据集所在地址
 #export OUTPUT=./output/search_spuall_3/
-export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/s_bert_distll_test_weight_att_disitill_head/
+export OUTPUT=/home/hadoop-aipnlp/cephfs/data/lidan65/distill/output/mnli/s_bert_distll_test_weight_att_disitill_head5/
 python3 sbert_distill.py \
   --task_name=mnli \
   --pooling_strategy=mean \
@@ -30,7 +30,7 @@ python3 sbert_distill.py \
   --use_kd_logit_mse=false \
   --kd_weight_logit=0.4 \
   --use_kd_att=true \
-  --kd_weight_att=10 \
+  --kd_weight_att=5 \
   --use_all_layer_emb=false \
   --use_resnet_predict=false \
   --use_weighted_att=true \
