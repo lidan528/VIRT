bd7d8481e9792884d690dbe063fd5d7026cb47d2
tanghongyin <tanghongyin@meituan.com>
2021-08-31 18:35:21 +0800

    add marco_distill.sh

--- marco_distill.py
+++ marco_distill.py
@@ -1985,7 +1985,6 @@ def main(_):
 
 
 if __name__ == "__main__":
-    flags.mark_flag_as_required("task_name")
     flags.mark_flag_as_required("vocab_file")
     flags.mark_flag_as_required("bert_config_file")
     flags.mark_flag_as_required("output_dir")
