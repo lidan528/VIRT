f0c36767163bb9ad786a23f25476cd5c78717f01
tanghongyin <tanghongyin@meituan.com>
2021-09-01 15:11:16 +0800

    add marco_distill.sh

--- marco_distill.sh
+++ marco_distill.sh
@@ -28,7 +28,7 @@ python marco_distill.py \
   --use_in_batch_neg=false \
   --use_kd_logit_mse=false \
   --kd_weight_logit=0.4 \
-  --use_kd_att=true \
+  --use_kd_att=false \
   --kd_weight_att=700 \
   --use_resnet_predict=true
 
