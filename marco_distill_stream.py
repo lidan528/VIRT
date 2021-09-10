5a0f5b5d50c4c74d0327691012194a58ac36fb7d
tanghongyin <tanghongyin@meituan.com>
2021-09-10 20:16:26 +0800

    disable tensor hook

--- marco_distill_stream.py
+++ marco_distill_stream.py
@@ -1153,15 +1153,15 @@ def model_fn_builder(bert_config,
 
             train_op = optimization.create_optimizer(
                 total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu, vars_student)
-            logging_hook = tf.train.LoggingTensorHook(
-                {"log_scores": log_scores},
-                every_n_iter=1
-            )
+            # logging_hook = tf.train.LoggingTensorHook(
+            #     {"log_scores": log_scores},
+            #     every_n_iter=1
+            # )
             output_spec = tf.contrib.tpu.TPUEstimatorSpec(
                 mode=mode,
                 loss=total_loss,
                 train_op=train_op,
-                training_hooks=[logging_hook],
+                # training_hooks=[logging_hook],
                 scaffold_fn=None)
         elif mode == tf.estimator.ModeKeys.EVAL:
 
